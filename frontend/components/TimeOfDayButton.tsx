d67fb9e40dcbfe7c4200f71b332f6e3f402e763d
Jenna Howieson <jenna@jennahowieson.co.uk>
2023-02-07 14:59:41 +0000

    time of day button component

---
+++ frontend/components/TimeOfDayButton.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { StyleSheet, TouchableOpacity, Text } from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+
+interface Props {
+    timeOfDayOption: string;
+    onPress: () => void;
+}
+
+const TimeOfDayButton = (props: Props) => {
+    const {timeOfDayOption, onPress} = props;
+    return (
+        <TouchableOpacity
+        onPress={onPress}
+        style={styles.container}
+        >
+        <Text style={styles.text }>{timeOfDayOption}
+            </Text>
+            </TouchableOpacity>
+    );
+    }
+
+    const styles = StyleSheet.create ({
+        container:{
+            backgroundColor: '#E0E1F0',
+            borderWidth: 1,
+            borderRadius: 10,
+            borderColor:'#25242B',
+            width: 333,
+            height: 72,
+            alignItems: 'center',
+            alignContent: 'center',
+            justifyContent:'center',
+            shadowColor: '#171717',
+            shadowOffset: {width: 0, height: 3},
+            shadowOpacity: 0.25,
+            shadowRadius: 2,
+            marginBottom:15,
+            marginTop:15
+        },
+        text: {
+            alignItems: 'center',
+            fontSize: 20,
+            borderRadius: 5,
+        }
+    })
+    export default TimeOfDayButton;
