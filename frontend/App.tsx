7a5283e2b9438ba4222ca6f400d6ca7ab6fe76ea
Jenna Howieson <jenna@jennahowieson.co.uk>
2023-02-07 11:55:50 +0000

    small button component

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,14 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import SmallButton from './components/SmallButton';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
