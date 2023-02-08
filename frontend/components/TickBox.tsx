5a8e0772d54e2ce2280c10b8618543953a04a5c3
RAnderson96 <rory.cameron.anderson@gmail.com>
2023-02-08 09:30:30 +0000

    added tickbox component

--- frontend/components/TickBox.tsx
+++ frontend/components/TickBox.tsx
@@ -1,5 +1,5 @@
-import { Text, StyleSheet, View, Image, TouchableOpacity} from 'react-native';
-import React, {useState} from 'react';
+import { StyleSheet, TouchableOpacity} from 'react-native';
+import React from 'react';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
 interface Props {
@@ -11,32 +11,29 @@ interface Props {
    
     const TickBox = (props: Props) => {
     const { value, onPress, size, backgroundColor } = props;
-    let iconSize: number = 22;
    return (
    <TouchableOpacity
      activeOpacity={0.7}
      style={[
        styles.checkBoxContainer,
        {
-         height: size || 38,
-         width: size || 38,
-         backgroundColor: "white",
+         height: size || 59,
+         width: size || 61,
+         backgroundColor: "white" || backgroundColor,
          borderColor: '#FFFFFF',
        },
      ]}
      onPress={onPress}
    >
 
-     {value ? <Ionicons name="checkmark-sharp" size={size ? size: iconSize} color="#FF914D" /> : <></>}
+     {value ? <Ionicons name="checkmark-sharp" style={styles.activeIcon} size={size ? 0.9*size : 54} color="#FF914D" /> : <></>}
    </TouchableOpacity>
    );
    };
    
    const styles = StyleSheet.create({
    activeIcon: {
-   width: 32,
-   height: 32,
-   tintColor: 'white',
+    margin: "auto"
    },
     checkBoxContainer: {
      borderRadius: 5,
--- frontend/screens/HomeScreen.tsx
+++ frontend/screens/HomeScreen.tsx
@@ -7,16 +7,10 @@ import TickBox from '../components/TickBox';
 import {useState} from 'react';
 
 export default function HomeScreen(){
-    const [isSelected, setSelection] = useState(false);
-    function onPress(){
-        setSelection(!isSelected)
-    }
-
+  
     return (
         <SafeAreaView style={styles.container}>
             <Text  style={styles.title} >Home view</Text>
-            
-            <TickBox value={isSelected} onPress={onPress} size={100} backgroundColor={'white'}  ></TickBox>
         </SafeAreaView>
     )
 
