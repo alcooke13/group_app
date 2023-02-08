225d2978bdc95452cf97bf646c006e0c87dad1d7
Edward Janson <99486978+edwardjanson@users.noreply.github.com>
2023-02-08 09:26:19 +0000

    added big plus component

---
+++ frontend/components/BigPlus.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { TouchableOpacity } from "react-native";
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+interface Props {
+    onPress?: () => void;
+}
+
+const BigPlus = (props: Props) => {
+
+    const {onPress} = props;
+
+    return (
+        <TouchableOpacity
+        onPress={onPress}
+        >
+            <Ionicons 
+            name="add-circle-sharp" 
+            size={60} 
+            color="#25E698"
+            backgroundColor="#25E698"
+            />
+        </TouchableOpacity>
+    );
+};
+
+export default BigPlus;
