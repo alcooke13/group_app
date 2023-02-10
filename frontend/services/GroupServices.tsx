02ab9207ae8f03e417a676c3d7225d4e69a7072e
Edward Janson <99486978+edwardjanson@users.noreply.github.com>
2023-02-10 15:48:15 +0000

    added crud functionality to group services

--- frontend/services/GroupServices.tsx
+++ frontend/services/GroupServices.tsx
@@ -26,3 +26,26 @@ export function getGroupData(): Promise<GroupData[]> {
 			return response as GroupData[];
 		});
 }
+
+export function postGroup(payload: JSON): Promise<JSON> {
+	return fetch('http://127.0.0.1:8080/groups?user_id=1', {
+        method: 'POST',
+        body: JSON.stringify(payload),
+        headers: { 'Content-Type': 'application/json' }
+	})
+		.then((response) => response.json());
+}
+
+export function updateGroupDataWithNewUsers(groupId: number, payload: JSON): Promise<Response> {
+	return fetch('http://127.0.0.1:8080/groups/' + groupId.toString() + '/add-users', {
+		method: 'PUT',
+        body: JSON.stringify(payload),
+        headers: { 'Content-Type': 'application/json' }
+	})
+}
+
+export function deleteGroup(groupId: number): Promise<Response> {
+    return fetch('http://127.0.0.1:8080/groups/' + groupId.toString(), {
+        method: 'DELETE'
+    });
+}
